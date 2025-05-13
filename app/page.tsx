7044bc45512046cb03a33f910363553c329062e7
UltimateHobbyCoder <ultimatehobbycoder@tutamail.com>
2025-05-13 05:57:21 +0000

    removed link and image cos unused

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
-import { Link } from "lucide-react";
 
 export default function Home() {
   const [messages, setMessages] = useState([
